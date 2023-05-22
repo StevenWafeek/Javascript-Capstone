77983829442f5904af087f61e72124828f914d35
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-05-22 23:42:21 +0300

    Add webpack

---
+++ dist/print.bundle.js
@@ -0,0 +1,18 @@
+(self["webpackChunkkanban_test"] = self["webpackChunkkanban_test"] || []).push([["print"],{
+
+/***/ "./src/print.js":
+/*!**********************!*\
+  !*** ./src/print.js ***!
+  \**********************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/print.js"));
+/******/ }
+]);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+      },
+      devtool: 'inline-source-map',
+      devServer: {
+        static: './dist',
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+      ],
+       output: {
+        filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
