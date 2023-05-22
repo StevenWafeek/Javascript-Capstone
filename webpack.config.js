c17b103aa3b6acc9b85a7fb437044e05b2d0400d
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-05-23 00:14:11 +0300

    Fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-        print: './src/print.js',
-      },
-      devtool: 'inline-source-map',
-      devServer: {
-        static: './dist',
-      },
-      plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development',
-        }),
-      ],
-       output: {
-        filename: '[name].bundle.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: '/',
