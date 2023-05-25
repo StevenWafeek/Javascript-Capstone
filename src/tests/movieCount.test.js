571b78dd79025ee40f0bc803675d67b030ed96a8
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-05-26 01:46:22 +0300

    Update moviecount test

--- src/tests/movieCount.test.js
+++ src/tests/movieCount.test.js
@@ -1,7 +1,7 @@
 import updateMovieCardCount from '../modules/movieCount.js';
 
 describe('updateMovieCardCount', () => {
-  test('should update the movie card count', () => {
+  test('should update the movie card count when there are movies in the DOM', () => {
     const movieCardCount = document.createElement('span');
     movieCardCount.id = 'span';
     document.body.appendChild(movieCardCount);
@@ -9,6 +9,21 @@ describe('updateMovieCardCount', () => {
     updateMovieCardCount(5);
 
     expect(movieCardCount.textContent).toEqual('5');
+
+    document.body.removeChild(movieCardCount);
+  });
+
+  test('should handle the case where there are no movies in the DOM', () => {
+    const movieCardCount = document.createElement('span');
+    movieCardCount.id = 'span';
+    document.body.appendChild(movieCardCount);
+
+    const movieItems = document.querySelectorAll('.movie-item');
+    movieItems.forEach((item) => item.remove());
+
+    updateMovieCardCount(0);
+
+    expect(movieCardCount.textContent).toEqual('0');
 
     document.body.removeChild(movieCardCount);
   });
